95e50ff002321f373b57b75f57aa9247f49a20db
Graham Watson <graham@phantomwatson.com>
2016-07-20 18:17:10 -0400

    Add jquery.jplayer.min.js to copy_vendor_files

--- gulpfile.js
+++ gulpfile.js
@@ -22,19 +22,19 @@ gulp.task('default', ['less', 'js', 'php', 'watch']);
  **************/
 
 gulp.task('php', function(callback) {
-	return runSequence('php_cs', 'php_unit', callback);
+    return runSequence('php_cs', 'php_unit', callback);
 });
 
 gulp.task('php_cs', function() {
-	return gulp.src(['src/**/*.php'])
-		// Validate files using PHP Code Sniffer
-		.pipe(phpcs({
-			bin: 'C:/xampp/htdocs/macc/vendor/bin/phpcs.bat',
-			standard: 'PSR2',
-			warningSeverity: 0
-		}))
-		// Log all problems that was found
-		.pipe(phpcs.reporter('log'));
+    return gulp.src(['src/**/*.php'])
+        // Validate files using PHP Code Sniffer
+        .pipe(phpcs({
+            bin: 'C:/xampp/htdocs/macc/vendor/bin/phpcs.bat',
+            standard: 'PSR2',
+            warningSeverity: 0
+        }))
+        // Log all problems that was found
+        .pipe(phpcs.reporter('log'));
 });
 
 gulp.task('php_unit', function() {
@@ -50,12 +50,12 @@ var srcJsFiles = [
 ];
 
 gulp.task('js', function(callback) {
-	return runSequence('js_lint', callback);
+    return runSequence('js_lint', callback);
 });
 
 gulp.task('js_lint', function () {
-	return gulp.src(srcJsFiles)
-    	.pipe(jshint())
+    return gulp.src(srcJsFiles)
+        .pipe(jshint())
         .pipe(jshint.reporter(stylish))
         .pipe(notify('JS linted'));
 });
@@ -69,7 +69,9 @@ gulp.task('copy_vendor_files', function () {
     gulp.src('vendor/twbs/bootstrap/dist/fonts/*')
         .pipe(gulp.dest('webroot/fonts'));
     gulp.src('vendor/twbs/bootstrap/dist/js/*')
-    .pipe(gulp.dest('webroot/bootstrap/js'));
+        .pipe(gulp.dest('webroot/bootstrap/js'));
+    gulp.src('vendor/happyworm/jplayer/dist/jplayer/jquery.jplayer.min.js')
+        .pipe(gulp.dest('webroot/js/jplayer'));
 });
 
 
@@ -78,9 +80,9 @@ gulp.task('copy_vendor_files', function () {
  *    LESS    *
  **************/
 gulp.task('less', function () {
-	var cleanCSSPlugin = new LessPluginCleanCSS({advanced: true});
-	gulp.src('webroot/css/style.less')
-		.pipe(less({plugins: [cleanCSSPlugin]}))
+    var cleanCSSPlugin = new LessPluginCleanCSS({advanced: true});
+    gulp.src('webroot/css/style.less')
+        .pipe(less({plugins: [cleanCSSPlugin]}))
         .pipe(gulp.dest('webroot/css'))
         .pipe(notify('LESS compiled'));
 });
@@ -93,19 +95,19 @@ gulp.task('less', function () {
 
 gulp.task('watch', function() {
     // LESS
-	var cleanCSSPlugin = new LessPluginCleanCSS({advanced: true});
-	watchLess('webroot/css/style.less', ['less'])
-    	.pipe(less({plugins: [cleanCSSPlugin]}))
+    var cleanCSSPlugin = new LessPluginCleanCSS({advanced: true});
+    watchLess('webroot/css/style.less', ['less'])
+        .pipe(less({plugins: [cleanCSSPlugin]}))
         .pipe(gulp.dest('webroot/css'))
         .pipe(notify('LESS compiled'));
 
-	// PHP
-	gulp.watch('src/**/*.php', ['php_cs', 'php_unit']);
-	gulp.watch('src/**/*.ctp', ['php_unit']);
-	
-	// JavaScript
+    // PHP
+    gulp.watch('src/**/*.php', ['php_cs', 'php_unit']);
+    gulp.watch('src/**/*.ctp', ['php_unit']);
+
+    // JavaScript
     gulp.watch(srcJsFiles, ['js']);
-	
+
     // Vendor files
     var vendorFiles = [
         'vendor/twbs/bootstrap/dist/fonts/*',
